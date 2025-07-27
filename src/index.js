d034b3ec5a820a845b03586b159b936c1c72024e
Fadonougbo <fadougbogautier@gmail.com>
2025-07-27 07:44:23 +0100

    feat: Simplify HTML structure and enhance CSS for header functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import {define} from "@mindemangou/magiccomponents"
 
 
-const x={['username']:'doe'}
 
-console.log(x.ville)
+let startScrollPosition=window.scrollY
+
+const header=document.querySelector('header')
+
+const headerHeight=header.offsetHeight
+    console.log( ((window.innerHeight+startScrollPosition)/document.body.offsetHeight ) * 100 )
+
+window.addEventListener('scroll',()=>{
+    
+    const newScrollPosition=window.scrollY
+    // if(newScrollPosition>startScrollPosition){
+    //     header.style.setProperty('transform',`translateY(-${headerHeight}px)`)
+    //     console.log('up')
+    // }else {
+    //     header.style.setProperty('transform',`translateY(0)`)
+    //     console.log('down')
+    // }
+
+    startScrollPosition=newScrollPosition
+})
+
+
 
 // const res=await fetch('http://localhost:8000',{
 //     signal:AbortSignal.timeout(3000)
